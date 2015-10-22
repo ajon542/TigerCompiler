095e232050bbbd89624ed8e61bf1bf2b73613802
Andrew Jones <ajon542@gmail.com>
2015-10-21 17:19:32 -0700

    Added some example parse trees for strings in the language

--- TigerCompiler/Program.cs
+++ TigerCompiler/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace TigerCompiler
 {
+    using System;
+
     // Grammar for the language.
     //
     // Statement -> Statement ; Statement               (CompoundStatement)
@@ -21,14 +23,61 @@ namespace TigerCompiler
     {
         static void Main(string[] args)
         {
-            CompoundStatement stm = new CompoundStatement(new AssignStatement("a",
+            /*CompoundStatement stm = new CompoundStatement(new AssignStatement("a",
                 new OpExpression(new NumExpression(5), BinOp.Plus, new NumExpression(3))),
                 new CompoundStatement(new AssignStatement("b",
                 new SeqExpression(new PrintStatement(new PairExpressionList(new IdExpression("a"),
                 new LastExpressionList(new OpExpression(new IdExpression("a"), BinOp.Minus,
                 new NumExpression(1))))),
                 new OpExpression(new NumExpression(10), BinOp.Times, new IdExpression("a")))),
-                new PrintStatement(new LastExpressionList(new IdExpression("b")))));
+                new PrintStatement(new LastExpressionList(new IdExpression("b")))));*/
+
+            // Example 1:
+            //
+            //              CompoundStm
+            //               /       \
+            //          PrintStm   PrintStm
+            //             /           \
+            //       PairExpList     LastExpList
+            //          /     \          \
+            //    NumExp   LastExpList  NumExp
+            //        /       /            \
+            //       |     NumExp           |
+            //       |       |              |
+            //       1       2              3
+            //
+            // print(1, 2) ; print(3)
+            //
+            CompoundStatement example1 = new CompoundStatement(
+                new PrintStatement(new PairExpressionList(new NumExpression(1), new LastExpressionList(new NumExpression(2)))),
+                new PrintStatement(new LastExpressionList(new NumExpression(3))));
+            Console.WriteLine(example1);
+
+            // Example 2:
+            //
+            //              CompoundStm
+            //               /       \
+            //          PrintStm   PrintStm
+            //             /           \
+            //       PairExpList        \_
+            //         /      |            \_
+            //        /       |               \
+            //       /        |                \
+            //      /     PairExpList       LastExpList
+            //     /      /       \              \
+            //    /   NumExp   LastExpList      NumExp
+            //   /       |         |              |
+            // NumExp    |       NumExp           |
+            //  |        |         |              |
+            //  1        2         3              4
+            //
+            // print(1, 2, 3) ; print(4)
+            //
+            CompoundStatement example2 = new CompoundStatement(
+                new PrintStatement(new PairExpressionList(new NumExpression(1), 
+                                                          new PairExpressionList(new NumExpression(2), new LastExpressionList(new NumExpression(3))))),
+                new PrintStatement(new LastExpressionList(new NumExpression(4))));
+            Console.WriteLine(example2);
 
             for (; ; ) { }
         }
