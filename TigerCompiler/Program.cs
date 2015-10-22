7fefdee11d9393f1d15c9a78a8a0db5b474c0e95
Andrew Jones <ajon542@gmail.com>
2015-10-22 11:16:47 -0700

    layout the CompoundStatements a bit more nicely. Also implemented some basic tree traversal to count the print arguments. Not working 100% yet, it is counting the arguments of the OpExpression as part of the print argument, where as these should be calculated as a single argument.

--- TigerCompiler/Program.cs
+++ TigerCompiler/Program.cs
@@ -1,7 +1,10 @@
 ﻿
+using System.Diagnostics;
+
 namespace TigerCompiler
 {
     using System;
+    using System.Collections.Generic;
 
     // Grammar for the language.
     //
@@ -21,40 +24,31 @@ namespace TigerCompiler
 
     // Tree-like Structure.
     //
-    // ------------------------------------- Statements -------------------------------------------
+    // ------------------------------------- [ Statements ] -------------------------------------------
     // 
     //           CompoundStatement         AssignStatement          PrintStatement
     //             /            \                 |                        |
     //        Statement      Statement        Expression            ExpressionList
     //
     //
-    // ------------------------------------- Expresssions -----------------------------------------
+    // ------------------------------------- [ Expresssions ] -----------------------------------------
     //
     //           OpExpression             SeqExpression         IdExpression      NumExpression
     //            /        \               /         \                |                 | 
     //      Expression  Expression    Statement  Expression        string              int
     //
     // 
-    // ------------------------------------- ExpressionList ---------------------------------------
+    // ------------------------------------- [ ExpressionList ] ---------------------------------------
     //
     //           PairExpressionList      LastExpressionList
     //              /          \                 |
     //        Expression  ExpressionList     Expression
     //
-    //---------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------
     class Program
     {
         static void Main(string[] args)
         {
-            /*CompoundStatement stm = new CompoundStatement(new AssignStatement("a",
-                new OpExpression(new NumExpression(5), BinOp.Plus, new NumExpression(3))),
-                new CompoundStatement(new AssignStatement("b",
-                new SeqExpression(new PrintStatement(new PairExpressionList(new IdExpression("a"),
-                new LastExpressionList(new OpExpression(new IdExpression("a"), BinOp.Minus,
-                new NumExpression(1))))),
-                new OpExpression(new NumExpression(10), BinOp.Times, new IdExpression("a")))),
-                new PrintStatement(new LastExpressionList(new IdExpression("b")))));*/
-
             // Example 1:
             //
             //              CompoundStm
@@ -76,7 +70,16 @@ namespace TigerCompiler
                 new PrintStatement(new LastExpressionList(new NumExpression(3))));
             Console.WriteLine(example1);
 
-            Traverse(example1);
+            // The idea behind the stack is as follows.
+            // If we encounter a PrintStatement:
+                // Pop the value on top of stack if it exists and increment it.
+                // Push that value back on the stack.
+                // Push a new count of 0 on top of stack.
+            // If we encounter a leaf node, we increment the value on top of stack if it exists.
+
+            int maxCount = 0;
+            Traverse(example1, new Stack<int>(), ref maxCount);
+            Console.WriteLine(maxCount);
 
             // Example 2:
             //
@@ -103,7 +106,10 @@ namespace TigerCompiler
                                                           new PairExpressionList(new NumExpression(2), new LastExpressionList(new NumExpression(3))))),
                 new PrintStatement(new LastExpressionList(new NumExpression(4))));
             Console.WriteLine(example2);
-            Traverse(example2);
+
+            maxCount = 0;
+            Traverse(example2, new Stack<int>(), ref maxCount);
+            Console.WriteLine(maxCount);
 
 
             // Example 3:
@@ -131,24 +137,139 @@ namespace TigerCompiler
                     new PrintStatement(new LastExpressionList(new NumExpression(1))), new NumExpression(2)))),
                 new PrintStatement(new LastExpressionList(new NumExpression(3))));
             Console.WriteLine(example3);
-            Traverse(example3);
+
+            maxCount = 0;
+            Traverse(example3, new Stack<int>(), ref maxCount);
+            Console.WriteLine(maxCount);
+
+            // Example 4:
+            CompoundStatement example4 =
+                new CompoundStatement(
+                    new AssignStatement(
+                        "a",
+                        new OpExpression(
+                            new NumExpression(5), 
+                            BinOp.Plus, 
+                            new NumExpression(3)
+                        )
+                    ),
+                new CompoundStatement(
+                    new AssignStatement(
+                        "b",
+                        new SeqExpression(
+                            new PrintStatement(
+                                new PairExpressionList(
+                                    new IdExpression("a"),
+                                    new LastExpressionList(
+                                        new OpExpression(
+                                            new IdExpression("a"),
+                                            BinOp.Minus,
+                                            new NumExpression(1)
+                                        )
+                                    )
+                                )
+                            ),
+                            new OpExpression(
+                                new NumExpression(10),
+                                BinOp.Times,
+                                new IdExpression("a")
+                            )
+                        )
+                    ),
+                    new PrintStatement(
+                        new LastExpressionList(
+                            new IdExpression("b")
+                        )
+                    )
+                )
+            );
+            Console.WriteLine(example4);
+
+            maxCount = 0;
+            Traverse(example4, new Stack<int>(), ref maxCount);
+            Console.WriteLine(maxCount);
+
+            // Example 5:
+            CompoundStatement example5 =
+                new CompoundStatement(
+                new PrintStatement(
+                    new LastExpressionList(
+                        new OpExpression(
+                            new SeqExpression(
+                                new PrintStatement(
+                                    new LastExpressionList(
+                                        new NumExpression(1)
+                                        )
+                                    ),
+                                new NumExpression(2)
+                                ),
+                            BinOp.Plus,
+                            new SeqExpression(
+                                new PrintStatement(
+                                    new LastExpressionList(
+                                        new NumExpression(3)
+                                        )
+                                    ),
+                                new NumExpression(4)
+                                )
+                            )
+                        )
+                    ),
+                new PrintStatement(
+                    new LastExpressionList(
+                        new NumExpression(5)
+                        )
+                    )
+                );
+
+            Console.WriteLine(example5);
 
             Console.WriteLine("\nPress any key to quit...");
             Console.ReadKey(true);
         }
 
-        public static void Traverse(Node node)
+        public static void Traverse(Node node, Stack<int> argCount, ref int maxCount)
         {
             if(node == null)
             {
                 return;
             }
 
-            Traverse(node.left);
-            
-            Console.WriteLine("[Node] {0}", node.GetType());
-            
-            Traverse(node.right);
+            if (node.GetType() == typeof (PrintStatement))
+            {
+                if (argCount.Count > 0)
+                {
+                    int count = argCount.Pop();
+                    count++;
+                    argCount.Push(count);
+                }
+                argCount.Push(0);
+            }
+
+            if (node.left == null && node.right == null)
+            {
+                // Leaf node found.
+                if (argCount.Count > 0)
+                {
+                    // We are an argument of a print statement.
+                    int count = argCount.Pop();
+                    count++;
+                    argCount.Push(count);
+                }
+            }
+
+            Traverse(node.left, argCount, ref maxCount);
+            //Console.WriteLine("[Node] {0} -> {1}", node.GetType(), node);
+            Traverse(node.right, argCount, ref maxCount);
+
+            if (node.GetType() == typeof (PrintStatement))
+            {
+                int count = argCount.Pop();
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
         }
     }
 }
